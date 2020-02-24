37b78c970377f7eb628660956bc71bde74b6c1c9
alexile <obsmyprof@gmail.com>
2020-02-24 20:35:36 +0200

    fix: 🐛 fix matrix interface

--- packages/numes/src/utils/validations.ts
+++ packages/numes/src/utils/validations.ts
@@ -1,13 +1,13 @@
 import {getSize} from './matrix'
 
-export const sameSize = (firstNdarray: Ndarray, ...restNdarrays: Ndarray[]): boolean => {
-    const {rows, cols} = getSize(firstNdarray)
-    return !restNdarrays.some(ndarray => {
-        const size = getSize(ndarray)
+export const sameSize = (xMatrix: Matrix, ...restMatrix: Matrix[]): boolean => {
+    const {rows, cols} = getSize(xMatrix)
+    return !restMatrix.some(matrix => {
+        const size = getSize(matrix)
         return size.cols !== cols || size.rows !== rows
     })
 }
 
-export const validNdarray = (...input: any[]): boolean => {
+export const validNdarray = (...input: Matrix[]): boolean => {
     return !input.some(item => !(Array.isArray(item) && Array.isArray(item[0])))
 }
