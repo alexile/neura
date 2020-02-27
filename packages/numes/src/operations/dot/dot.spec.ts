f7ffe84ecc4b53fe7690bb88cc3813196ff6c43c
alexile <obsmyprof@gmail.com>
2020-02-27 18:17:51 +0200

    feat: 🎸 add basic dot product

--- packages/numes/src/operations/dot/dot.spec.ts
+++ packages/numes/src/operations/dot/dot.spec.ts
@@ -4,8 +4,8 @@ describe('dot', () => {
     it('should multiply two matrices and return a dot product', () => {
         const xMatrix = [[1, 2, 3], [4, 5, 6]]
         const yMatrix = [[7, 8], [9, 10], [11, 12]]
-        // const resultNdarray = [[58, 64], [139, 154]]
+        const result = [[58, 64], [139, 154]]
 
-        expect(dot(xMatrix, yMatrix)).toBeTruthy()
+        expect(dot(xMatrix, yMatrix)).toStrictEqual(result)
     })
 })
--- packages/numes/src/operations/dot/index.ts
+++ packages/numes/src/operations/dot/index.ts
@@ -1,5 +1,20 @@
+const dotMatricies = (xMatrix: Matrix, yMatrix: Matrix): Matrix => {
+    const result: Matrix = []
+    for (let i = 0; i < xMatrix.length; i++) {
+        result[i] = []
+        for (let j = 0; j < yMatrix[0].length; j++) {
+            let sum = 0
+            for (let k = 0; k < xMatrix[0].length; k++) {
+                sum += xMatrix[i][k] * yMatrix[k][j]
+            }
+            result[i][j] = sum
+        }
+    }
+    return result
+}
+
 const dot = (...input: Matrix[]) => {
-    return input
+    return input.reduce(dotMatricies)
 }
 
 export default dot
